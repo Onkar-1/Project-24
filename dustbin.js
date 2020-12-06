b3011620613763cfa05a18cf5049b0a37d34ddc7
Onkar-1 <71157521+Onkar-1@users.noreply.github.com>
2020-12-06 17:00:01 -0500

    Add files via upload

---
+++ dustbin.js
@@ -0,0 +1,56 @@
+class dustbin{
+	constructor(x,y){
+		this.x=x;
+		this.y=y;
+		this.dustbinWidth=200;
+		this.dustbinHeight=100;
+		this.wallThickness=20;
+		this.angle=0;	
+		
+		this.bottomBody=Bodies.rectangle(this.x, this.y, this.dustbinWidth, this.wallThickness, {isStatic:true})
+		this.leftWallBody=Bodies.rectangle(this.x-this.dustbinWidth/2, this.y-this.dustbinHeight/2, this.wallThickness, this.dustbinHeight, {isStatic:true})
+		Matter.Body.setAngle(this.leftWallBody, this.angle);
+		
+
+		this.rightWallBody=Bodies.rectangle(this.x+this.dustbinWidth/2, this.y-this.dustbinHeight/2, this.wallThickness, this.dustbinHeight, {isStatic:true})
+		Matter.Body.setAngle(this.rightWallBody, -1*this.angle);
+		World.add(world, this.bottomBody)
+		World.add(world, this.leftWallBody)
+		World.add(world, this.rightWallBody);
+
+	}
+	display(){
+			var posB=this.bottomBody.position;
+			var posL=this.leftWallBody.position;
+			var posR=this.rightWallBody.position;
+
+			push()
+			translate(posL.x, posL.y);
+			rectMode(CENTER)
+			angleMode()
+			fill(255)
+			stroke(255)
+			rotate(this.angle)
+			rect(0,0,this.wallThickness, this.dustbinHeight);
+			pop()
+
+			push()
+			translate(posR.x, posR.y);
+			rectMode(CENTER)
+			stroke(255)
+			angleMode()
+			fill(255)
+			rotate(-1*this.angle)
+			rect(0,0,this.wallThickness, this.dustbinHeight);
+			pop()
+
+			push()
+			translate(posB.x, posB.y);
+			rectMode(CENTER)
+			stroke(255)
+			angleMode()
+			fill(255)
+			rect(0,0,this.dustbinWidth, this.wallThickness);
+			pop()
+	}
+}
